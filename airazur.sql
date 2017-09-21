35954f8c86a8ffe8ee1b04c6882ceb7924a17f64
rlJoshua <31963935+rlJoshua@users.noreply.github.com>
2017-09-21 16:14:36 +0200

    Add files via upload

--- airazur.sql
+++ airazur.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 21 Septembre 2017 à 09:11
+-- Généré le :  Jeu 21 Septembre 2017 à 14:14
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -31,6 +31,14 @@ CREATE TABLE `avion` (
   `nomavion` varchar(30) DEFAULT NULL,
   `capmax` int(3) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `avion`
+--
+
+INSERT INTO `avion` (`numavion`, `nomavion`, `capmax`) VALUES
+(1, 'AIR037', 100),
+(2, 'BOEING119', 300);
 
 -- --------------------------------------------------------
 
@@ -72,11 +80,20 @@ CREATE TABLE `vol` (
   `nomvol` varchar(15) DEFAULT NULL,
   `depart` varchar(30) DEFAULT NULL,
   `arrivee` varchar(30) DEFAULT NULL,
-  `datedepart` date NOT NULL,
-  `datearrive` date NOT NULL,
+  `datedepart` varchar(30) DEFAULT NULL,
+  `datearrive` varchar(30) DEFAULT NULL,
   `place` int(3) DEFAULT NULL,
-  `idavion` int(3) DEFAULT NULL
+  `idavion` int(3) DEFAULT NULL,
+  `prix` float DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `vol`
+--
+
+INSERT INTO `vol` (`numvol`, `nomvol`, `depart`, `arrivee`, `datedepart`, `datearrive`, `place`, `idavion`, `prix`) VALUES
+('1', 'AIR5007', 'Paris CDG - France', 'Dakar - Sénégal', '22/04/2011 12:00', '22/04/2011 17:00', 120, 1, 560),
+('2', 'AIR5108', 'Paris CDG - France', 'Dakar - Sénégal', '23/04/2011 13:00', '23/04/2011 18:20', 120, 2, 600);
 
 --
 -- Index pour les tables exportées
@@ -107,6 +124,15 @@ ALTER TABLE `vol`
   ADD PRIMARY KEY (`numvol`),
   ADD KEY `fk_avi_vol` (`idavion`);
 
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `numutil` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
