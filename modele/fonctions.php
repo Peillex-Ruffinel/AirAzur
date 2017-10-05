41bfda9193aa09a902cc835bddb46816c1c698a9
rlJoshua <31963935+rlJoshua@users.noreply.github.com>
2017-10-05 15:39:07 +0200

    Update fonctions.php

--- modele/fonctions.php
+++ modele/fonctions.php
@@ -2,9 +2,7 @@
 function getLesVols() {
 $vols = array();
 $v = 0;
-
 include 'pdoConnect.php';
-
 try {
     $rep = $bdd->prepare('SELECT * FROM vol');
     $rep->execute(); 
@@ -25,28 +23,22 @@ try {
 }
  // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
     array_push($vols, $lesVols);
-
 // Retourner le tableau
-
 return $vols;
 }
-
 function reserverVol () {
     // rÃ©cup numÃ©ro vol
     $numero = $_REQUEST["numero"];
     return $numero;
 }
-
 function initPanier() {
     if(!isset($_SESSION['reservations'])) {
 	$_SESSION['reservations']= array();
     }
 }
-
 function ajouterAuPanier($reservation) {    
     $_SESSION['reservations'][]= $reservation;
 }
-
 function validerReservation() {
     $reservation = array();
     $reservation["numero"] =  $numero;
@@ -66,7 +58,6 @@ function validerReservation() {
     $reservation["nbVoyageur"] =  $nbVoyageur;
     
     include 'pdoConnect.php';
-
     /*try {
         $insertion = $bdd->prepare('INSERT INTO reservation ()
 						VALUES (\'\', :, :, :, :)');
@@ -82,32 +73,29 @@ function validerReservation() {
     
     return $reservation;
 }
-
 function getLesReservations() {
     $reserv = array();
     $r = 0;
-
     include 'pdoConnect.php';
-
     try {
-        $rep = $bdd->prepare('SELECT * FROM ');
+        $rep = $bdd->prepare('SELECT nom, prenom, idvol, nbplace FROM reserver 
+        JOIN utilisateur ON reserver.idutil=utilisateur.numutil
+        JOIN vol ON reserver.idvol=vol.numvol');
+        
         $rep->execute(); 
         while ($donnees = $rep->fetch(PDO::FETCH_ASSOC)) {
             $lesReserv[$r] = array(
-            $donnees['numvol'],
-            $donnees['depart'], 
-            $donnees['arrivee'], 
-            $donnees['datedepart'], 
-            $donnees['datearrive'], 
-            $donnees['place'], 
-            $donnees['idavion'], 
-            $donnees['prix']);
+            $donnees['nom'],
+            $donnees['prenom'], 
+            $donnees['idvol'], 
+            $donnees['nbplace']);
         $r++;
     }
-    } catch (Exception $e) {
-        die ('Erreur :' . $e ->getMessage());
-    }
+} catch (Exception $e) {
+    die ('Erreur :' . $e ->getMessage());
+}
+ // Remplissage du tableau multi-dimensionnel $vols avec chacun des vols
     array_push($reserv, $lesReserv);
-
-    return $reserv;
+// Retourner le tableau
+return $reserv;
 }
