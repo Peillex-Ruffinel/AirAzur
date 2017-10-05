9d4efcdeb8a7ac631d74afd0a311f95298898758
rlJoshua <31963935+rlJoshua@users.noreply.github.com>
2017-10-05 10:24:05 +0200

    Update v_reservation.php

--- vues/v_reservation.php
+++ vues/v_reservation.php
@@ -1,8 +1,29 @@
+<html>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
+
+<div id="contenue">
+    <form>
+        <fieldset>
+            <legend>Réservation n°</legend>
+            <label>Nom</label>
+            <input type='text' name='nom' maxlength='30' placeholder='Votre nom'><br />
+            <label>Prénom</label>
+            <input type='text' name='prenom' maxlength='30' placeholder='Votre prénom'><br />
+            <label>E-mail</label>
+            <input type='email' name='mail' placeholder='Votre mail'><br />
+            <label>Adresse</label>
+            <input type='text' name='adresss' maxlength='100' placeholder='Votre adresse'><br />
+            <label>Code postale</label>
+            <input type='text' name='cp' maxlength='5' placeholder='Code Postale'><br />
+            <label>Ville</label>
+            <input type='text' name='ville' maxlength='30' placeholder='Ville'><br />
+            <label>Nombre de places</label>
+            <input type='number' value=1 name='place'><br />
+            <input type='submit' value='Réserver'>
+            <input type='reset' value='Annuler'><br />
+        </fieldset>
+    </form>
+</div>
 
