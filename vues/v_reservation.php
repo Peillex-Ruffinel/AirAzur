fbd6a74e5827f897d1d4b3dcf42d2eccdfccdccf
rlJoshua <31963935+rlJoshua@users.noreply.github.com>
2017-10-05 15:37:53 +0200

    Update v_reservation.php

--- vues/v_reservation.php
+++ vues/v_reservation.php
@@ -1,29 +1,23 @@
 <html>
-<?php
+    <table>
+        <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Numéro de vol</td>h
+            <th>Nombre de places</th>
+            <th>PDF</th>
+        </tr>
+        <?php
+            foreach($LesRevervations as $i =>$unRev) {
 
-?>
-
-<div id="contenue">
-    <form>
-        <fieldset>
-            <legend>Réservation n°</legend>
-            <label>Nom</label>
-            <input type='text' name='nom' maxlength='30' placeholder='Votre nom'><br />
-            <label>Prénom</label>
-            <input type='text' name='prenom' maxlength='30' placeholder='Votre prénom'><br />
-            <label>E-mail</label>
-            <input type='email' name='mail' placeholder='Votre mail'><br />
-            <label>Adresse</label>
-            <input type='text' name='adresss' maxlength='100' placeholder='Votre adresse'><br />
-            <label>Code postale</label>
-            <input type='text' name='cp' maxlength='5' placeholder='Code Postale'><br />
-            <label>Ville</label>
-            <input type='text' name='ville' maxlength='30' placeholder='Ville'><br />
-            <label>Nombre de places</label>
-            <input type='number' value=1 name='place'><br />
-            <input type='submit' value='Réserver'>
-            <input type='reset' value='Annuler'><br />
-        </fieldset>
-    </form>
-</div>
-
+               echo"
+                <tr>
+                    <td>$unRev[0]</td>
+                    <td>$unRev[1]</td>
+                    <td>$unRev[2]</td>
+                    <td>$unRev[3]</td>
+                    <td><a href='index.php'><img src='image/pdf.png'/></a></td>
+                </tr>";
+            }
+        ?>
+    </table>
